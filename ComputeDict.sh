afaba9a33ba50184edf3dc3f8fb24805e1781956
kapilmahajan123 <montyempire123gmail.com>
2022-09-01 15:25:00 +0530

    [Kapil]-program of compute value of dictionary

---
+++ ComputeDict.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+declare -A Computation
+
+echo "Enter Value a"
+read a
+echo "Enter Value b"
+read b
+echo "Enter Value c"
+read c
+
+result1=$(( $a + $b * $c))
+echo "result1 is=" $result1
+
+result2=$(( $a * $b + $c))
+echo "result2 is=" $result2
+
+result3=$(( $c + $a / $b ))
+echo "result3 is=" $result3
+
+result4=$(( $a % $b + $c))
+echo "result4 is=" $result4
+
+#storing Values in Dictionary
+Computation[result1]=$result1
+Computation[result2]=$result2
+Computation[result3]=$result3
+Computation[result4]=$result4
